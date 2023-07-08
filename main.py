5932a20f67f96cf1c7d60eab078c70f0ebcbb51f
Avital Tchernoguz <57183118+avitaltch@users.noreply.github.com>
2023-07-08 16:12:30 +0300

    Lesson 1 Python Files

---
+++ main.py
@@ -0,0 +1,6 @@
+first = 7
+second = 44.3
+
+print(f"Add first and second: { first + second }")
+print(f"Multiply first and second: { first * second }")
+print(f"Divide first and second: { first / second }")
